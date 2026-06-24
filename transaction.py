ade8a5a37eaafa9fb7dfe11115fb708f33b30a86
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-23 18:12:51 -0700

    add json persistence

--- transaction.py
+++ transaction.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+import json
 
 @dataclass
 class Transaction:
@@ -15,8 +16,24 @@ class Transaction:
 
 transactions = []
 
+def load_transactions():
+    global transactions
+    try:
+        with open('budget_data.json', 'r') as f:
+            data = json.load(f)
+            transactions = [Transaction(**item) for item in data]
+    except FileNotFoundError:
+        transactions = []
+
+def save_transactions():
+    with open('budget_data.json', 'w') as f:
+        json.dump([t.__dict__ for t in transactions], f, default=str)
+
 def add_transaction(transaction):
     transactions.append(transaction)
+    save_transactions()
 
 def list_transactions():
     return transactions
+
+load_transactions()
